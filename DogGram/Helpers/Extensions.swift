2cae38e83d35934872269ba4f1986f99983bf9d3
Robert Basamac <rbasamac@gmail.com>
2022-11-16 21:51:07 +0200

    Add a Tab Bar using TabView

---
+++ DogGram/Helpers/Extensions.swift
@@ -0,0 +1,26 @@
+//
+//  Extensions.swift
+//  DogGram
+//
+//  Created by Robert Basamac on 16.11.2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    struct MyTheme {
+        static var purpleColor: Color {
+            return Color("PurpleColor")
+        }
+        
+        static var yellowCollor: Color {
+            return Color("ColorYellow")
+        }
+        
+        static var beigeColor: Color {
+            return Color("ColorBeige")
+        }
+    }
+}
---
+++ DogGram/Views/Screens/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  DogGram
+//
+//  Created by Robert Basamac on 16.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            Text("Screen 1")
+                .tabItem {
+                    Label("Feed", systemImage: "book.fill")
+                }
+            
+            Text("Screen 2")
+                .tabItem {
+                    Label("Browse", systemImage: "magnifyingglass")
+                }
+            
+            Text("Screen 3")
+                .tabItem {
+                    Label("Upload", systemImage: "square.and.arrow.up.fill")
+                }
+            
+            Text("Screen 4")
+                .tabItem {
+                    Label("Profile", systemImage: "person.fill")
+                }
+        }
+        .tint(Color.MyTheme.purpleColor)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
