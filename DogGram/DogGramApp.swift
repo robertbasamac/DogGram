c7a5f24bdbadd9c08d38c0468b25b6c11edec828
Robert Basamac <rbasamac@gmail.com>
2022-11-16 20:56:58 +0200

    Initial Commit

---
+++ DogGram/DogGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogGramApp.swift
+//  DogGram
+//
+//  Created by Robert Basamac on 16.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct DogGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
