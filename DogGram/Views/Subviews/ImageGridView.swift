3894391da292f781a4cf7afe445c6c44bb4575ec
Robert Basamac <rbasamac@gmail.com>
2022-11-27 12:39:55 +0200

    Adding a custom heart animation to like posts (LikeAnimationView)

--- DogGram/Views/Subviews/ImageGridView.swift
+++ DogGram/Views/Subviews/ImageGridView.swift
@@ -20,7 +20,7 @@ struct ImageGridView: View {
                 NavigationLink {
                     FeedView(posts: PostArrayObject(post: post), title: "Post")
                 } label: {
-                    PostView(post: post, showHeaderAndFooter: false)
+                    PostView(post: post, showHeaderAndFooter: false, addHeartAnimationToView: false)
                 }
             }
         }
--- DogGram/Views/Subviews/PostView.swift
+++ DogGram/Views/Subviews/PostView.swift
@@ -12,6 +12,9 @@ struct PostView: View {
     @State var post: PostModel
     var showHeaderAndFooter: Bool
     
+    @State var animateLike: Bool = false
+    @State var addHeartAnimationToView: Bool
+    
     var body: some View {
         VStack(alignment: .center, spacing: 0) {
             
@@ -42,14 +45,29 @@ struct PostView: View {
             }
             
             // MARK: Image
-            Image("dog1")
-                .resizable()
-                .scaledToFit()
+            ZStack {
+                Image("dog1")
+                    .resizable()
+                    .scaledToFit()
+                
+                if addHeartAnimationToView {
+                    LikeAnimationView(animate: $animateLike)
+                }
+            }
             
             // MARK: Footer
             if showHeaderAndFooter {
                 HStack(alignment: .center, spacing: 20) {
-                    Image(systemName: "heart")
+                    Button {
+                        if post.likedByUser {
+                            unlikePost()
+                        } else {
+                            likePost()
+                        }
+                    } label: {
+                        Image(systemName: post.likedByUser ? "heart.fill" : "heart")
+                    }
+                    .tint(post.likedByUser ? .red : .primary)
                     
                     NavigationLink {
                         CommentsView()
@@ -72,13 +90,29 @@ struct PostView: View {
             }
         }
     }
+    
+    // MARK: Functions
+    private func likePost() {
+        let updatedPost = PostModel(id: post.id, postID: post.postID, userID: post.userID, username: post.username, caption: post.caption, dateCreated: post.dateCreated, likeCount: post.likeCount + 1, likedByUser: true)
+        self.post = updatedPost
+        
+        animateLike = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            animateLike = false
+        }
+    }
+    
+    private func unlikePost() {
+        let updatedPost = PostModel(id: post.id, postID: post.postID, userID: post.userID, username: post.username, caption: post.caption, dateCreated: post.dateCreated, likeCount: post.likeCount - 1, likedByUser: false)
+        self.post = updatedPost
+    }
 }
 
 struct PostView_Previews: PreviewProvider {
     static var post: PostModel = PostModel(postID: "", userID: "", username: "Tanjiro", caption: "This is a test caption", dateCreated: Date(), likeCount: 0, likedByUser: false)
     
     static var previews: some View {
-        PostView(post: post, showHeaderAndFooter: true)
+        PostView(post: post, showHeaderAndFooter: true, addHeartAnimationToView: true)
             .previewLayout(.sizeThatFits)
     }
 }
